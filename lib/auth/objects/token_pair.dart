01ceab104bc64d2dc24fde23512f4f60a2974453
y4hoo58 <yigit.yahya58@gmail.com>
2026-04-08 19:13:21 +0300

    Refactor TokenPair class to extend Equatable for improved equality comparison

--- lib/auth/objects/token_pair.dart
+++ lib/auth/objects/token_pair.dart
@@ -1,7 +1,12 @@
+import 'package:equatable/equatable.dart';
+
 /// Holds an access token and refresh token pair.
-class TokenPair {
+class TokenPair extends Equatable {
   final String accessToken;
   final String refreshToken;
 
   const TokenPair({required this.accessToken, required this.refreshToken});
+
+  @override
+  List<Object?> get props => [accessToken, refreshToken];
 }
